e892395f7ec53d698cc000037e94ec3ac08b1154
Nivrox <nicopro.git@gmail.com>
2025-10-30 02:10:18 -0300

    saco importacion del index

--- PagOmdB/src/main.jsx
+++ PagOmdB/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Index.css'
 import App from './App.jsx'
 import { DataProvider } from './Context/DataContext.jsx'
 import 'react-toastify'
